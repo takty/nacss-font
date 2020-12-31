565b0de883adbbcaba8942641ae538d87d1b656c
Takuto Yanagida <yanagida@stxst.com>
2020-12-31 17:08:58 +0900

    Initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('sass', () => gulp.src(['src/**/*.scss'])
 	.pipe($.rename({ extname: '.min.css' }))
 	.pipe($.sourcemaps.write('.'))
 	.pipe(gulp.dest('./dist'))
-	.pipe(gulp.dest('./docs'))
+	.pipe(gulp.dest('./docs/css'))
 );
 
 gulp.task('watch', () => {
@@ -30,3 +30,39 @@ gulp.task('watch', () => {
 gulp.task('build', gulp.parallel('sass'));
 
 gulp.task('default', gulp.series('build', 'watch'));
+
+
+// -----------------------------------------------------------------------------
+
+
+gulp.task('docs-sass', gulp.series('sass', () => gulp.src(['docs/style.scss'])
+	.pipe($.plumber({
+		errorHandler: function (err) {
+			console.log(err.messageFormatted);
+			this.emit('end');
+		}
+	}))
+	.pipe($.sourcemaps.init())
+	.pipe($.dartSass({ outputStyle: 'compressed' }))
+	.pipe($.autoprefixer({ remove: false }))
+	.pipe($.rename({ extname: '.min.css' }))
+	.pipe($.sourcemaps.write('.'))
+	.pipe(gulp.dest('./docs/css')))
+);
+
+// gulp.task('docs-js', gulp.series('js-with-option', () => gulp.src(['dist/js/stile-full.min.js', 'dist/js/stile-full.min.js.map'])
+// 	.pipe($.plumber())
+// 	.pipe(gulp.dest('./docs')))
+// );
+
+gulp.task('docs-watch', () => {
+	// gulp.watch('src/js/**/*.js',     gulp.series('docs-js'));
+	gulp.watch('src/sass/**/*.scss', gulp.series('docs-sass'));
+	gulp.watch('docs/*.scss',        gulp.series('docs-sass'));
+});
+
+gulp.task('docs-build', gulp.parallel('docs-sass'));
+
+gulp.task('docs-default', gulp.series('docs-build', 'docs-watch'));
+
+gulp.task('docs', gulp.parallel('default', 'docs-default'));
